347a88eb080b259fba42385eb4a113dc2a3432ce
nonobromo <noamab22@gmail.com>
2024-12-31 10:53:29 +0200

    added

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ connect();
 async function connect() {
   try {
     await mongoose
-      .connect("mongodb://localhost:27017/serverSideProject")
+      .connect(
+        "mongodb+srv://noamab22:p2kspCA3Io0669uY@cluster0.m2gxd.mongodb.net/projectAPI"
+      )
       .then(() => {
         console.log("connected to the database");
         app.listen(PORT, () => {
